cb8b29178d08907cd7bed58ad0020de3cade76bf
midefaux <michel.defaux@gmail.com>
2023-11-03 17:20:43 -0500

    second committed

--- parcel-project-template-main/src/js/01-gallery.js
+++ parcel-project-template-main/src/js/01-gallery.js
@@ -1,5 +1,8 @@
+
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const lightbox = new SimpleLightbox('.my-gallery');
 console.log(galleryItems);
