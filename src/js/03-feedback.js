22f9ba2addf5d4c54522523779cc2094b4326132
midefaux <michel.defaux@gmail.com>
2023-11-13 19:58:07 -0500

    Last correction

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-import throttle from './lodash.throttle';
+import throttle from 'lodash.throttle';
 
 const form = documenuerySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
